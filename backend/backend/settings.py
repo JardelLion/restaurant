6cbecf49ed40ac8e3f0b611fa1d24797cdcd81fd
Jardel Lion <businessjbx@gmail.com>
2024-12-03 21:00:57 +0200

    adde settins url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-8ub-m)pl5$6$j=m4#zevj-^_a_laaf971v&w0l+y!l-$d%s4uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['restaurant-xm7h.onrender.com', 'localhost', '127.0.0.1', '[::1]',
+                 
+        'http://localhost:3000']
+
 
 
 # Application definition
