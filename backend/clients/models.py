c56e01389e0ce0fd1574876561e0230371ff9c95
Jardel Lion <businessjbx@gmail.com>
2024-12-03 21:37:06 +0200

    adicionando o birthday

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -7,12 +7,13 @@ class Client(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False
     )
-    
+
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
     send_notification = models.BooleanField(default=False)
     address = models.TextField(blank=True, null=True)
+    birthday = models.DateTimeField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
